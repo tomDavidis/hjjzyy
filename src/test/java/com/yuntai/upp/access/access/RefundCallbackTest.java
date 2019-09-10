37dc4d5e7d0d88379587eba0c8656abb860be47f
金仁 <963920633@qq.com>
2019-09-10 15:57:46 +0800

    2019年09月10日15:56:59

--- src/test/java/com/yuntai/upp/access/access/RefundCallbackTest.java
+++ src/test/java/com/yuntai/upp/access/access/RefundCallbackTest.java
@@ -10,6 +10,8 @@ import com.yuntai.upp.client.config.cache.CacheInstance;
 import com.yuntai.upp.client.fresh.export.access.ClientReceiver;
 import com.yuntai.upp.client.fresh.model.dto.refundcallback.RefundCallbackDto;
 import com.yuntai.upp.sdk.enums.BizType;
+import com.yuntai.upp.sdk.enums.ChannelProductType;
+import com.yuntai.upp.sdk.enums.ChannelType;
 import com.yuntai.upp.sdk.enums.TradeStatus;
 import com.yuntai.upp.sdk.interfaces.Signable;
 import com.yuntai.upp.sdk.util.SignUtil;
@@ -71,12 +73,13 @@ public class RefundCallbackTest {
                 .isvId(20000L)
                 .bizType(BizType.REGISTER.getCode())
                 .bizId(UUIDUtil.create())
+                .channelType(ChannelType.ALI.getCode())
+                .channelProduct(ChannelProductType.ALI_BAR_CODE.getCode())
                 .requestNo(UUIDUtil.create())
                 .refundNo(UUIDUtil.create())
                 .refundFee(BigDecimal.ONE)
                 .tradeStatus(TradeStatus.REFUND_SUCCESS.getCode())
                 .outRefundNo(UUIDUtil.create())
-                .inRefundNo(UUIDUtil.create())
                 .refundTime(LocalDateTime.now())
                 .bizData(null)
                 .build();
